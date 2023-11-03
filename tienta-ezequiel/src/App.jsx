dd1e1382c70627e4cdec57ce6c9e2d3ef0f2b96c
ºezequielmau <ezecerdan08@gmail.com>
2023-11-02 21:39:29 -0300

    mi e.c

--- tienta-ezequiel/src/App.jsx
+++ tienta-ezequiel/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from './componets/NavBar'
-import ItemListContainer from './componets/ItemListContainer'
-function App() {
-  const [count, setCount] = useState(0)
 
-  function App() {
-    return (
-      <div>
-        <NavBar />
-        <ItemListContainer greeting="Bienvenido a nuestra tienda en línea" />
-      </div>
-    );
-  }}
-  export default App;
+import { useState } from 'react';
+import { Header } from './componets/Header';
+import { ProductList } from './componets/ProductList';
+
+function App() {
+	const [allProducts, setAllProducts] = useState([]);
+	const [total, setTotal] = useState(0);
+	const [countProducts, setCountProducts] = useState(0);
+
+	return (
+		<>
+			<Header
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+			<ProductList
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+		</>
+	);
+}
+
+export default App;
---
+++ tienta-ezequiel/src/data.js
@@ -0,0 +1,38 @@
+export const data=[
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1579586337278-3befd40fd17a?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D",
+        nameProduct: "Galaxy Watch3 Bluetooth (45mm) Titanium",
+        price: 79.999 ,
+        quantity: 1},
+
+        {id: 2,
+        img: "https://images.unsplash.com/photo-1546868871-7041f2a55e12?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D",
+        nameProduct: "Galaxy Watch4 Classic Bluetooth (46mm) Negro",
+        price: 225.749 ,
+        quantity: 1},
+
+        {id: 3,
+        img: "https://images.unsplash.com/photo-1544117519-31a4b719223d?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D",
+        nameProduct: "Galaxy Watch5 Pro Bluetooth (45mm) Gray",
+        price: 267.749,
+        quantity: 1},   
+
+        {id: 4,
+        img: "https://images.unsplash.com/photo-1637160151663-a410315e4e75?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNtYXJ0JTIwd2F0Y2h8ZW58MHx8MHx8fDA%3D",
+        nameProduct: "Galaxy Watch5 Bluetooth (44mm) Graphite",
+        price: 183.749 ,
+        quantity: 1}, 
+        
+        {id: 5,
+        img: "https://images.unsplash.com/photo-1575311373937-040b8e1fd5b6?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNtYXJ0JTIwd2F0Y2h8ZW58MHx8MHx8fDA%3D",
+        nameProduct: "Galaxy Watch5 Bluetooth (40mm) Graphite",
+        price: 167.999 ,
+        quantity: 1},   
+
+        {id: 6,
+        img: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHNtYXJ0JTIwd2F0Y2h8ZW58MHx8MHx8fDA%3D",
+        nameProduct: "Galaxy Watch4 Classic Bluetooth (42mm) Negro",
+        price: 99.999 ,
+        quantity: 1},
+];
